24f875b2b0f4b548fe8fca53f51692a3a78e90d7
Joseph Schilz <joseph@schilz.org>
2018-04-03 18:47:17 -0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<name>/')
+def hello_name(name):
+    return 'Hello, {}!'.format(name)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8523))
+    app.run(host='localhost', port=port)
+
